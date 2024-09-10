9da169c59d29a7b266b55d6d1f7e142f40665b12
mucyochris47 <mucyochris250@gmail.com>
2024-09-10 09:07:54 -0700

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) not in 'qe':
-        print(chr(letter), end="")
-
+i = 97
+while i <= 122:
+    if i == 113 or i == 101:
+        i += 1
+    else:
+        print("{0:c}".format(i), end="")
+        i += 1
