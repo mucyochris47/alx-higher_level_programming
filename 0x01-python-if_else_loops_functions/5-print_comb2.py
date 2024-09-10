3d5ab67586d315c6154986ff81f853f6b36ee665
mucyochris47 <mucyochris250@gmail.com>
2024-09-10 09:15:03 -0700

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
-for i in range(100):
-    print(f"{i:02}", end=", " if i < 99 else "\n")
+i = 0
+while i <= 99:
 
+    print("0{0:d}".format(i) if i < 10 else "{0:d}".format(i),
+          end=", " if i < 99 else "\n")
+
+    i += 1
