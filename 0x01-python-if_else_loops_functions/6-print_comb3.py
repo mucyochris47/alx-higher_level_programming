94157b8f2f3cf4613cbd7a0c0f96142db1b4f000
mucyochris47 <mucyochris250@gmail.com>
2024-09-10 09:33:02 -0700

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
-for f in range(0, 10):
-    for k in range(f + 1, 10):
-        print(f"{f:02}{k:02}", end=", " if not (f == 8 and k == 9) else "\n")
+i = 0
+j = 0
 
+while i < 10:
+    j = 0
+    while j < 10:
+        if (i != j and i < j):
+            print("{0:d}{1:d}".format(i, j), end="")
+            if (i >= 8 and j >= 9):
+                print()
+            else:
+                print(end=", ")
+        j += 1
+    i += 1
