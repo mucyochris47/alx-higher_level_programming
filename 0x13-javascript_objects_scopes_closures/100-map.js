83f108e4d62888fad44ba30b637ef4fbda429f3c
mucyochris47 <mucyochris250@gmail.com>
2024-12-19 10:12:01 -0800

    task 12

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
 
-const newList = list.map((val, idx) => val * idx);
+const list = require('./100-data').list;
 console.log(list);
+const newList = list.map((x, index) => x * index);
 console.log(newList);
