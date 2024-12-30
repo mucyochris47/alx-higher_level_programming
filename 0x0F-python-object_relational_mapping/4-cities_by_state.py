47b9d6fbe2b7659ec0b27ddd056db666a31b5c08
mucyochris47 <mucyochris250@gmail.com>
2024-12-30 02:48:36 -0800

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,39 +1,17 @@
-import sys
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    # Get command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to the MySQL database
-    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=db_name)
-    cursor = db.cursor()
-
-    # Query to get cities based on the state name
-    query = """
-    SELECT cities.id, cities.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id
-    """
-    
-    cursor.execute(query, (state_name,))
-
-    # Fetch all results
-    cities = cursor.fetchall()
-
-    # Print the result
-    if cities:
-        for city in cities:
-            print(f"{city[0]}: {city[1]}")
-    else:
-        print("No cities found for the state")
-
-    # Close the connection
-    cursor.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
     db.close()
-
