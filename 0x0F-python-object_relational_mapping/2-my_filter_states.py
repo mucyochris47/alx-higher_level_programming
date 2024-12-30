fb8f6d31611f983f9dcdd812617a738a701a9dec
mucyochris47 <mucyochris250@gmail.com>
2024-12-30 02:12:47 -0800

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-"""
-This script takes arguments and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument. The script
-is safe from SQL injection.
-"""
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    db_connect = MySQLdb.connect(
-        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
-    cursor = db_connect.cursor()
-    
-    # Use parameterized query to avoid SQL injection
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (argv[4],))
 
-    rows = cursor.fetchall()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-
-    cursor.close()
-    db_connect.close()
-
+    cur.close()
+    db.close()
